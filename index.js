0df9d42936c261554c167b8053aea4cc7414ceff
Andrew Kennedy <akennedy@cloudflare.com>
2015-04-13 12:02:48 -0700

    clean up code. fix rounding issue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var _ = require('lodash');
 var prefixes = require('binary-prefix');
 var sprintf = require('underscore.string/sprintf');
+var convert = require('./lib/convert');
 
 function parseOptions(options) {
   options = options || {};
@@ -22,44 +23,6 @@ function parseOptions(options) {
   };
 };
 
-
-var convert = function(value, units, inputUnit, outputUnit, decimal){
-  var unitKeys = _.keys(units);
-  var converted;
-  var k = decimal ? 1000 : 1024;
-  var i;
-
-  if (inputUnit) {
-    inputUnit = inputUnit.toUpperCase();
-  } else {
-    inputUnit = 'B';
-  }
-
-  i = _.indexOf(unitKeys, inputUnit);
-
-  if (i < 0){
-    i = 0;
-  }
-
-  value = value * Math.pow(k, i);
-
-  if (outputUnit) {
-    outputUnit = outputUnit.toUpperCase();
-    i = _.indexOf(unitKeys, outputUnit);
-  } else {
-    value = (value / Math.pow(k, i));
-    i = Math.floor(Math.log(value) / Math.log(k));
-  }
-
-  converted = (value / Math.pow(k, i));
-
-  return {
-    value: converted, 
-    unit: i < unitKeys.length ? units[unitKeys[i]] : units[_.last(unitKeys)]
-  };
-};
-
-
 module.exports = function(bytes, options){
   var config = parseOptions(options);
   var value;
@@ -73,28 +36,24 @@ module.exports = function(bytes, options){
     bytes = 0;
   }
 
-  if (bytes < 1){
-    return sprintf(config.outputFormat, (bytes).toString(), 'B');
-  }
-
   // convert to bytes
   if (config.convert.from !== 'B'){
-    var tmp = convert(bytes, config.units, config.convert.from, 'B', config.decimal);
-    bytes = tmp.value;
+    bytes = convert(bytes, config.units, config.convert.from, 'B', config.decimal).value;
   }
 
   if (bytes === 0) {
     value = 0;
-    unit = units.B;
-  } else {
-    var tmp = convert(bytes, config.units, 'B', config.convert.to, config.decimal);
-    value = tmp.value;
-    unit = tmp.unit;
+    unit = config.units.B;
+    return sprintf(config.outputFormat, value, unit);
   }
+
+  var converted = convert(bytes, config.units, 'B', config.convert.to, config.decimal);
+  value = converted.value;
+  unit = converted.unit;
 
   if (config.minSigFigs && value.toString().replace('.', '').length < config.minSigFigs){
     value = value.toPrecision(config.minSigFigs);
-  } else {
+  } else if (value > 1 || unit !== 'B') {
     value = value.toFixed(config.digits);
   }
 
