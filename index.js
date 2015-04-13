3079cf8e63e111787846e0cdb2d5e07cdb55d5a1
Andrew Kennedy <akennedy@cloudflare.com>
2015-04-13 00:20:08 -0700

    refactor to make more readable

--- index.js
+++ index.js
@@ -3,34 +3,67 @@ var _ = require('lodash');
 var prefixes = require('binary-prefix');
 var sprintf = require('underscore.string/sprintf');
 
-var convert = function(value, units, inputUnit, outputUnit, k){
-  inputUnit = inputUnit.toUpperCase();
-  outputUnit = outputUnit.toUpperCase();
-  var i = _.indexOf(units, inputUnit);
+function parseOptions(options) {
+  options = options || {};
+  var decimal = options.binary ? false : true;
+  return {
+    decimal: decimal,
+    outputFormat: options.format || '%s %s',
+    units: prefixes({
+      custom: options.units,
+      decimal: decimal
+    }),
+    digits: options.digits || 0,
+    significantDigits: null,
+    convert: {
+      from: options.from ? options.from.toUpperCase() : 'B',
+      to: options.to ? options.to.toUpperCase() : null
+    }
+  };
+};
+
+
+var convert = function(value, units, inputUnit, outputUnit, decimal){
+  var unitKeys = _.keys(units);
+  var converted;
+  var k = decimal ? 1000 : 1024;
+  var i;
+
+  if (inputUnit) {
+    inputUnit = inputUnit.toUpperCase();
+  } else {
+    inputUnit = 'B';
+  }
+
+  i = _.indexOf(unitKeys, inputUnit);
+
   if (i < 0){
     i = 0;
   }
+
   value = value * Math.pow(k, i);
-  i = _.indexOf(units, outputUnit);
-  return value / Math.pow(k, i);
+
+  if (outputUnit) {
+    outputUnit = outputUnit.toUpperCase();
+    i = _.indexOf(unitKeys, outputUnit);
+  } else {
+    value = (value / Math.pow(k, i));
+    i = Math.floor(Math.log(value) / Math.log(k));
+  }
+
+  converted = (value / Math.pow(k, i));
+
+  return {
+    value: converted, 
+    unit: i < unitKeys.length ? units[unitKeys[i]] : units[_.last(unitKeys)]
+  };
 };
 
+
 module.exports = function(bytes, options){
-  options = options || {};
-  var format = options.format || '%s %s';
-  var decimal = !options.binary;
-  var k = decimal ? 1000 : 1024;
-  var units = prefixes({
-    custom: options.units,
-    decimal: decimal
-  });
-  var unitKeys = _.keys(units);
-  var digits = options.digits || 0;
-  var inputUnit = options.from ? options.from.toUpperCase() : 'B';
-  var outputUnit = options.to ? options.to.toUpperCase() : null;
+  var config = parseOptions(options);
   var value;
   var unit;
-  var i;
 
   if (typeof bytes === 'string'){
     bytes = parseInt(bytes, 10);
@@ -41,28 +74,25 @@ module.exports = function(bytes, options){
   }
 
   if (bytes < 1){
-  return sprintf(format, (bytes).toString(), 'B');
+    return sprintf(config.outputFormat, (bytes).toString(), 'B');
   }
 
   // convert to bytes
-  if (inputUnit !== 'B'){
-    bytes = convert(bytes, unitKeys, inputUnit, 'B', k);
+  if (config.convert.from !== 'B'){
+    var tmp = convert(bytes, config.units, config.convert.from, 'B', config.decimal);
+    bytes = tmp.value;
   }
 
-  // convert from bytes to either the desired output unit or the highest unit prefix
-  if (outputUnit){
-    value = convert(bytes, unitKeys, 'B', outputUnit, k);
-    unit = units[outputUnit];
-  } else if (bytes === 0) {
+  if (bytes === 0) {
     value = 0;
-    unit = units[_.first(unitKeys)];
+    unit = units.B;
   } else {
-    i = Math.floor(Math.log(bytes) / Math.log(k));
-    value = (bytes / Math.pow(k, i));
-    unit = i < unitKeys.length ? units[unitKeys[i]] : units[_.last(unitKeys)];
+    var tmp = convert(bytes, config.units, 'B', config.convert.to, config.decimal);
+    value = tmp.value;
+    unit = tmp.unit;
   }
 
-  value = value.toFixed(digits);
+  value = value.toFixed(config.digits);
 
-  return sprintf(format, value, unit);
+  return sprintf(config.outputFormat, value, unit);
 };
