4c77f6339659714d58b53c989ecaba58dc936dcd
Andrew Kennedy <akennedy2292@gmail.com>
2015-07-23 09:18:09 -0700

    use older version of underscore.string

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 var _ = require('lodash');
 var prefixes = require('binary-prefix');
-var sprintf = require('underscore.string/sprintf');
+var sprintf = require('underscore.string').sprintf;
 var convert = require('./lib/convert');
 
 function parseOptions(options) {
